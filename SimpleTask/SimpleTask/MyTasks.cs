03cfad9a2bebbb3a91cfaaa763a7248402ffaf29
UNAPIMD <ExpIgn@gmail.com>
2024-10-04 11:15:29 +0300

    Add .gitignore and SimpleTask

---
+++ SimpleTask/SimpleTask/MyTasks.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SimpleTask
+{
+    static class MyTasks
+    {
+        public static void Print(string message) => Console.WriteLine(message);
+    }
+}
---
+++ SimpleTask/SimpleTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using static SimpleTask.MyTasks;
+
+namespace SimpleTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Print("Hello, world");
+        }
+    }
+}
